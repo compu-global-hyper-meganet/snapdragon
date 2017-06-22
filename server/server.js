e71984a3757931fa744c30d9d0b7c1d22d3c9501
dougajmcdonald <dougajmcdonald@gmail.com>
2017-06-22 13:37:30 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+
+app.use('static', express.static(path.join(__dirname + '/../client/ui/dist/static/')));
+
+console.log(path.join(__dirname + '/../client/ui/dist/static/'));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', {'root': '../client/ui/dist'});
+});
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
